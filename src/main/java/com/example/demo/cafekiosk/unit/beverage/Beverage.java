c98a0a41db214d196b8611f0f3c3a5ed1de36caf
2carrot84 <2carrot84@gmail.com>
2025-03-01 15:03:28 +0900

    feat,test: JUnit5 테스트하기
    sample 코드 생성 및 단위 테스트 코드 생성

---
+++ src/main/java/com/example/demo/cafekiosk/unit/beverage/Beverage.java
@@ -0,0 +1,6 @@
+package com.example.demo.cafekiosk.unit.beverage;
+
+public interface Beverage {
+  String getName();
+  int getPrice();
+}
---
+++ src/main/java/com/example/demo/cafekiosk/unit/order/Order.java
@@ -0,0 +1,13 @@
+package com.example.demo.cafekiosk.unit.order;
+
+import com.example.demo.cafekiosk.unit.beverage.Beverage;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Order {
+
+  private final LocalDateTime orderDateTime;
+  private final List<Beverage> beverages;
+}
